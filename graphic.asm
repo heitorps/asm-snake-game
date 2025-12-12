a409bd42e0eb9df9cfaab8466be44bd55246a62b
heitorps <heitorps2020@gmail.com>
2025-12-11 21:52:08 -0300

    initial commit

---
+++ graphic.asm
@@ -0,0 +1,87 @@
+;função cursor
+; Parametros:
+; 	DH = linha (0-29) e  DL = coluna  (0-79)
+global cursor:
+		PUSHf
+		PUSH 	AX
+		PUSH 	BX
+		PUSH	CX
+		PUSH	DX
+		PUSH	SI
+		PUSH	DI
+		PUSH	BP
+		MOV     AH,2
+		MOV     BH,0
+		INT     10h
+		POP		BP
+		POP		DI
+		POP		SI
+		POP		DX
+		POP		CX
+		POP		BX
+		POP		AX
+		POPf
+		RET
+
+
+
+;função caracter escrito na posição do cursor
+; Parametros:
+; 	AL = caracter a ser escrito
+; 	cor definida na variavel cor
+global caracter:
+		PUSHf
+		PUSH 	AX
+		PUSH 	BX
+		PUSH	CX
+		PUSH	DX
+		PUSH	SI
+		PUSH	DI
+		PUSH	BP
+    	MOV     AH,9
+    	MOV     BH,0
+    	MOV     CX,1
+   		MOV     bl,[cor]
+    	INT     10h
+		POP		BP
+		POP		DI
+		POP		SI
+		POP		DX
+		POP		CX
+		POP		BX
+		POP		AX
+		POPf
+		RET
+
+
+
+;função plot_xy
+; Parametros:
+;	PUSH x; PUSH y; CALL plot_xy;  (x<639, y<479)
+; 	cor definida na variavel cor
+global plot_xy:
+		PUSH	BP
+		MOV		BP,SP
+		PUSHf
+		PUSH 	AX
+		PUSH 	BX
+		PUSH	CX
+		PUSH	DX
+		PUSH	SI
+		PUSH	DI
+	    MOV     AH,0Ch
+	    MOV     AL,[cor]
+	    MOV     BH,0
+	    MOV     DX,479
+		SUB		DX,[BP+4]
+	    MOV     CX,[BP+6]
+	    INT     10h
+		POP		DI
+		POP		SI
+		POP		DX
+		POP		CX
+		POP		BX
+		POP		AX
+		POPf
+		POP		BP
+		RET		4
